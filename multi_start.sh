5d80954c4ba143d3bc1d194dcd715b754fa70bac
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-02 20:38:46 +0800

    update_multi_gpu

--- multi_start.sh
+++ multi_start.sh
@@ -1,12 +1,12 @@
 wandb login 3776ee3f88e548b933163a866f440f1d98a32b1d ;
-python -m torch.distributed.launch --nproc_per_node 8 /apdcephfs/private_luxiferchen/test_yolov5/train.py \
---weights /apdcephfs/private_luxiferchen/yolov5/yolov5s.pt \
---batch 256 \
---epochs 120 \
---data /apdcephfs/private_luxiferchen/test_yolov5/data/top23-p123-p1.yaml \
+python -m torch.distributed.launch --nproc_per_node 8 train.py \
+--weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s.pt \
+--batch 128 \
+--epochs 5 \
+--data ./Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
---name 'multiple_gpu_b256_ep120' \
---project '/apdcephfs/private_luxiferchen/top23-p123-p1/' \
---hyp /apdcephfs/private_luxiferchen/test_yolov5/data/hyps/hyp.scratch.yaml \
+--name multi_gpu_baseline \
+--project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
+--hyp ./Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml \
 --sync-bn \
---device 0,1,2,3,4,5,6,7
+--device 0,1,2,3,4,5,6,7 \
