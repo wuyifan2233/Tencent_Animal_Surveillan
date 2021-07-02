67c56a785939fc2b377d83d881a2b66313744528
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-02 21:19:44 +0800

    update

--- train_start.sh
+++ train_start.sh
@@ -1,10 +1,10 @@
 wandb login 3776ee3f88e548b933163a866f440f1d98a32b1d ;
-python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5ls.pt \
+python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s6.pt \
 --batch 64 \
 --epochs 120 \
 --img-size 1280 \
 --data ./Tencent_Animal_Surveillance/configs/top23-p123-p1.yaml \
 --cache \
---name single_gpu_baseline \
+--name single-b64-ep120-1231-5ls \
 --project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
 --hyp ./Tencent_Animal_Surveillance/data/hyps/hyp.scratch.yaml \
