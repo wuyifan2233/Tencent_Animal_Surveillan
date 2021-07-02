83fc37c85989293c2720ab8af192f0f3fe2ac1b6
jingyuanchan <jingyuanchen1423@gmail.com>
2021-07-02 20:03:11 +0800

    update

--- train_start.sh
+++ train_start.sh
@@ -1,9 +1,9 @@
 wandb login 3776ee3f88e548b933163a866f440f1d98a32b1d ;
-python train.py --weights /apdcephfs/private_luxiferchen/yolov5/yolov5s.pt \
---batch 128 \
---epochs 2 \
---data /apdcephfs/private_luxiferchen/test_yolov5/data/top23-p123-p1.yaml \
+python train.py --weights /apdcephfs/private_luxiferchen/Tencent_WWF/weights/yolov5s.pt \
+--batch 64 \
+--epochs 5 \
+--data top23-p123-p1.yaml \
 --cache \
---name single_gpu_delete \
---project /apdcephfs/private_luxiferchen/top23-p123-p1/ \
---hyp /apdcephfs/private_luxiferchen/latest_yolov5/data/hyps/hyp.scratch.yaml \
+--name single_gpu_baseline \
+--project /apdcephfs/private_luxiferchen/Tencent_WWF/training_projects/ \
+--hyp hyp.scratch.yaml \
